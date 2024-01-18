94abac9bc0337e1d6146196ea087656f0efe9f36
DavidKoens <david.koens@novi-education.nl>
2024-01-18 14:47:43 +0100

    database fix

--- src/database.py
+++ src/database.py
@@ -1,7 +1,6 @@
 import pymysql
 import os
 
-
 def connectToDatabase():
     database = None
     try:
@@ -15,9 +14,9 @@ def connectToDatabase():
             cursorclass=pymysql.cursors.DictCursor
         )
     except:
-        db = databaseConfig['database']
-        host = databaseConfig['host']
-        print(f'Unable to connect to database \'{db}\' on \'{host}\'')
+        # db = databaseConfig['database']
+        # host = databaseConfig['host']
+        print(f'Unable to connect to database')
     finally:
         return database
     
@@ -37,23 +36,3 @@ def getData(sqlQuery):
         except:
             print('Unable to close connection to database')
         return result
-    
-def insertData(abp, priority, date, time, postcode):
-    connection = connectToDatabase()
-    
-    try:
-        cursor = connection.cursor()
-
-        sql = f'INSERT INTO meldingen (ABP, Prioriteit, Datum, Tijd, Postcode) VALUES (%s, %s, %s, %s, %s);'
-        val = (abp, priority, date, time, postcode)
-        cursor.execute(sql, val)
-
-        connection.commit()
-        print(f'{cursor.rowcount} records inserted')
-    except:
-        print(f'Unable to insert data into meldingen: \n\t{abp}\n\t{priority}\n\t{date}\n\t{time}\n\t{postcode}')
-    finally:
-        try:
-            connection.close()
-        except:
-            print("Unable to close connection to database")
