b1c9d52899135639ecd040d57908d16712686990
mathijs-hg <153609489+mathijs-hg@users.noreply.github.com>
2024-01-18 13:56:26 +0100

    database test

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+import src.database as database
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"NOVI CICD werkt!!"}
+
+
+@app.get("/db")
+def get_data():
+    query = "SELECT * FROM meldingen;"
+    data = database.getData(query)
+    return{data[0]}
