6830d2b19d53927395a7a222874922f1a548e263
RubenMeul <153607951+RubenMeul@users.noreply.github.com>
2024-01-25 14:44:41 +0100

    Started tests

---
+++ test_database.py
@@ -0,0 +1,37 @@
+import os
+import sys
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from models.base import Base
+
+SQLALCHEMY_DATABASE_URL = f'mysql://{os.environ.get("MYSQL_USER")}:{os.environ.get("MYSQL_PASSWORD")}@{os.environ.get("MYSQL_HOST")}:{os.environ.get("MYSQL_PORT")}/{os.environ.get("MYSQL_DB")}'
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def init_db():
+    from models.p2000Message import P2000Message
+
+    Base.metadata.create_all(bind=engine)
+    seed_db()
+
+def seed_db():
+    print('Seeding DB')
+
+    from models.p2000Message import P2000Message as message
+
+    db = SessionLocal()
+
+    if db.query(message).count == 0:
+        messages = [
+            message(Tijd='12:00:00',Datum='2101-05-10',Regio='Adam',ABP='Politie',Prioriteit=1,Capcode='221231'),
+            message(Tijd='14:00:00',Datum='2001-07-10',Regio='Rdam',ABP='Ambulance',Prioriteit=1,Capcode='421230'),
+            message(Tijd='15:00:00',Datum='1101-01-10',Regio='Edam',ABP='Brandweer',Prioriteit=2,Capcode='521237'),
+            message(Tijd='19:00:00',Datum='2010-06-10',Regio='Zaandam',ABP='Politie',Prioriteit=3,Capcode='921233')
+        ]
+        db.add_all(messages)
+        db.commit()
+    db.close()
---
+++ test_server.py
@@ -0,0 +1,33 @@
+import os
+import sys
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+import logging 
+import pytest
+
+#current_dir = os.path.dirname(os.path.abspath(__file__))
+#parent_dir = os.path.dirname(current_dir)
+#sys.path.append(parent_dir)
+
+from main import app
+from test_database import init_db
+from models.base import Base
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = f'mysql://{os.environ.get("MYSQL_USER")}:{os.environ.get("MYSQL_PASSWORD")}@{os.environ.get("MYSQL_HOST")}:{os.environ.get("MYSQL_PORT")}/{os.environ.get("MYSQL_DB")}'
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+
+client = TestClient(app)
+
+@pytest.fixture()
+def test_db():
+    Base.metadata.create_all(bind=engine)
+    init_db()
+    yield
+    Base.metadata.drop_all(bind=engine)
+
+def test_read_main(test_db):
+    response = client.get('/messages/')
+    assert response.status_code == 200
