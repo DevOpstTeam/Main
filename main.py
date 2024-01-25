260853bd384885d5a5c13207237ccd6dab775f57
mathijs-hg <153609489+mathijs-hg@users.noreply.github.com>
2024-01-25 11:07:31 +0100

    demo

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def get_db():
 
 @app.get("/")
 def read_root():
-    return {"NOVI CICD werkt!!!!!"}
+    return {"hoi mensen"}
 
 @app.get("/messages")
 def read_messages(db=Depends(get_db)) -> list[messageSchema]:
