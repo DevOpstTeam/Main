67fcec67936aa08f3ca7c5d34caa546f5d710c08
RubenMeul <ruben.meulenbroek@novi-education.nl>
2024-01-27 11:39:57 +0100

    poging 4? idk

--- test_database.py
+++ test_database.py
@@ -1,5 +1,4 @@
 import os
-import sys
 
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
@@ -7,7 +6,7 @@ from sqlalchemy.orm import sessionmaker
 
 from models.base import Base
 
-SQLALCHEMY_DATABASE_URL = f'mysql://{os.environ.get("MYSQL_USER")}:{os.environ.get("MYSQL_PASSWORD")}@{os.environ.get("MYSQL_HOST")}:{os.environ.get("MYSQL_PORT")}'
+SQLALCHEMY_DATABASE_URL = f'mysql://{os.environ.get("MYSQL_USER")}:{os.environ.get("MYSQL_PASSWORD")}@{os.environ.get("MYSQL_HOST")}:{os.environ.get("MYSQL_PORT")}/{os.environ.get("MYSQL_DB")}'
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- test_server.py
+++ test_server.py
@@ -1,13 +1,9 @@
 import os
-import sys
+
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
 import logging 
 import pytest
-
-#current_dir = os.path.dirname(os.path.abspath(__file__))
-#parent_dir = os.path.dirname(current_dir)
-#sys.path.append(parent_dir)
 
 from main import app
 from test_database import init_db
@@ -16,7 +12,7 @@ from models.base import Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = f'mysql://{os.environ.get("MYSQL_USER")}:{os.environ.get("MYSQL_PASSWORD")}@{os.environ.get("MYSQL_HOST")}:{os.environ.get("MYSQL_PORT")}'
+SQLALCHEMY_DATABASE_URL = f'mysql://{os.environ.get("MYSQL_USER")}:{os.environ.get("MYSQL_PASSWORD")}@{os.environ.get("MYSQL_HOST")}:{os.environ.get("MYSQL_PORT")}/{os.environ.get("MYSQL_DB")}'
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 client = TestClient(app)
