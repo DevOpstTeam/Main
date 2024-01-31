9af2ae2ddcd33853492779011b485201cf5197b0
RubenMeul <ruben.meulenbroek@novi-education.nl>
2024-01-31 15:22:53 +0100

    added secrets to test.yml

--- test_server.py
+++ test_server.py
@@ -13,3 +13,15 @@ def test_read_main():
 def test_read_messages():
     response = client.get("/messages")
     assert response.status_code == 200
+
+def test_read_message_12():
+    response = client.get("/messages/12")
+    assert response.status_code == 200
+    assert response.json() == {'ABP': 'AMBU A2 Ambu 06154 VWS GROENLO Rit 24566 ',
+                               'Capcode': '0820154 MKA N-O Gelderland ( Ambulance 06-154 )\n',
+                               'Datum': '2022-01-24T00:00:00',
+                               'Prioriteit': 1,
+                               'Regio': 'Noord en Oost Gelderland',
+                               'Tijd': '12:45:09',
+                               'id': 12
+                               }
