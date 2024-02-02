ba786b654c6f54e45628ac2fb7cf6f5065b1375a
RubenMeul <ruben.meulenbroek@novi-education.nl>
2024-02-02 15:41:29 +0100

    added bunch of tests

--- test_server.py
+++ test_server.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
-from src.dbSeeder import seedLocal
+from sqlalchemy.orm import Session
+from src.models.p2000Message import P2000Message as message
+from src.schemas.p2000Message import P2000MessageCreate, P2000Message
+import src.dbSeeder
 
 from main import app
+from main import get_db
 
 seedLocal = True
 client = TestClient(app)
@@ -31,7 +35,70 @@ def test_non_existing():
     response = client.get("/messages/-1")
     assert response.status_code == 404
 
-# def test_ceate():
-#     return
+def test_post_patch_delete():
+    #create test message
+    message_data = {
+         "Datum": "9999-02-16",
+         "Tijd": "12:33:12",
+         "ABP": "TEST",
+         "Prioriteit": 2,
+         "Regio": "TEST",
+         "Capcode": "999999",
+    }
+
+    #post test message
+    response = client.post("/messages/", json=message_data)
+    assert response.status_code == 201
     
+    #get data base and set message id
+    db: Session = next(get_db())
+    db_message = db.query(message).filter_by(Capcode=message_data["Capcode"]).first()
+    message_id = db_message.id
+    assert db_message is not None
+    
+    #modify test message
+    # updated_message_data = {
+    #      "Datum": "9999-02-16",
+    #      "Tijd": "12:33:12",
+    #      "ABP": "TEST_2",
+    #      "Prioriteit": 2,
+    #      "Regio": "TEST",
+    #      "Capcode": "999999",
+    # }
+
+    # patch_response = client.patch(f"/messages/{message_id}", json=updated_message_data)
+    # assert patch_response.status_code == 200
+    
+    # #update database 
+    # db: Session = next(get_db())
+    # updated_message = db.query(message).filter_by(Capcode=message_data["Capcode"]).first()
+    # assert updated_message is not None
+    # assert updated_message.ABP == updated_message_data["ABP"]
+    # assert updated_message.Capcode == updated_message_data["Capcode"]
+
+    #delete test message
+    del_response = client.delete(f"/messages/{message_id}")
+    assert del_response.status_code == 200 
+    
+    #make sure test message is gone
+    response_but_again = client.get(f"/messages/{message_id}") 
+    assert response_but_again.status_code == 404
+
+def test_filter():
+    # Test case with all parameters
+    response = client.get("/messages/filter/?dateStart=01-01-2023&dateEnd=01-02-2023&timeStart=12:00&timeEnd=13:00&abp=test&priority=1&region=test&capcode=123")
+    assert response.status_code == 200
+
+    # Test case with only dateStart parameter
+    response = client.get("/messages/filter/?dateStart=01-01-2023")
+    assert response.status_code == 200
+
+    # Test case with dateStart + priority
+    response = client.get("/messages/filter/?dateStart=02-01-2023&priority=2")
+    assert response.status_code == 200
+
+    # Test case with invalid priority
+    response = client.get("/messages/filter/?priority=b")
+    assert response.status_code == 422
+
 seedLocal = False
