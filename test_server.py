2f6c88361fc436c5a6ba3367d55d685afbcaba26
RubenMeul <ruben.meulenbroek@novi-education.nl>
2024-02-05 13:57:39 +0100

    added more tests

--- test_server.py
+++ test_server.py
@@ -8,21 +8,27 @@ import src.dbSeeder
 from main import app
 from main import get_db
 
+#set database to local database
 seedLocal = True
 client = TestClient(app)
 
 def test_read_main():
+    #get default message
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == ['0 down time cicd werkt!']
 
 def test_read_messages():
+    #get all messages
     response = client.get("/messages")
     assert response.status_code == 200
 
 def test_read_message_12():
+    #get message with id 12
     response = client.get("/messages/12")
     assert response.status_code == 200
+    
+    #check if found message has correct data   
     assert response.json() == {'ABP': 'AMBU A2 Ambu 06154 VWS GROENLO Rit 24566 ',
                                'Capcode': '0820154 MKA N-O Gelderland ( Ambulance 06-154 )\n',
                                'Datum': '2022-01-24',
@@ -32,6 +38,7 @@ def test_read_message_12():
                                'id': 12}
 
 def test_non_existing():
+    #get non existend message
     response = client.get("/messages/-1")
     assert response.status_code == 404
 
@@ -56,16 +63,17 @@ def test_post_patch_delete():
     message_id = db_message.id
     assert db_message is not None
     
-    #modify test message
+    # #create updated message
     # updated_message_data = {
-    #      "Datum": "9999-02-16",
-    #      "Tijd": "12:33:12",
-    #      "ABP": "TEST_2",
-    #      "Prioriteit": 2,
-    #      "Regio": "TEST",
-    #      "Capcode": "999999",
+    #     "Datum": "9999-02-16",
+    #     "Tijd": "12:33:12",
+    #     "ABP": "TEST_2",
+    #     "Prioriteit": 2,
+    #     "Regio": "TEST",
+    #     "Capcode": "999999"
     # }
 
+    # #modify test message
     # patch_response = client.patch(f"/messages/{message_id}", json=updated_message_data)
     # assert patch_response.status_code == 200
     
@@ -85,20 +93,21 @@ def test_post_patch_delete():
     assert response_but_again.status_code == 404
 
 def test_filter():
-    # Test case with all parameters
+    #test case with all parameters
     response = client.get("/messages/filter/?dateStart=01-01-2023&dateEnd=01-02-2023&timeStart=12:00&timeEnd=13:00&abp=test&priority=1&region=test&capcode=123")
     assert response.status_code == 200
 
-    # Test case with only dateStart parameter
+    #test case with only dateStart parameter
     response = client.get("/messages/filter/?dateStart=01-01-2023")
     assert response.status_code == 200
 
-    # Test case with dateStart + priority
+    #test case with dateStart + priority
     response = client.get("/messages/filter/?dateStart=02-01-2023&priority=2")
     assert response.status_code == 200
 
-    # Test case with invalid priority
+    #test case with invalid priority
     response = client.get("/messages/filter/?priority=b")
     assert response.status_code == 422
 
+#set database to not local database
 seedLocal = False
