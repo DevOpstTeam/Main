77e1f26d5535fc9140ee5a2500b0374779b35f80
DavidKoens <david.koens@novi-education.nl>
2024-02-05 12:06:39 +0100

    Changed tests so they make use of a local test database

--- test_server.py
+++ test_server.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-from main import app
+from main import app, get_db
 
+def get_test_db():
+    """Get a reference to the local test database and close the database when finished."""
+    engine = create_engine("sqlite:///test_messages.db")
+    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+app.dependency_overrides[get_db] = get_test_db
 client = TestClient(app)
 
 def test_read_main():
@@ -17,12 +31,12 @@ def test_read_messages():
 def test_read_message_12():
     response = client.get("/messages/12")
     assert response.status_code == 200
-    assert response.json() == {'ABP': 'AMBU A2 Ambu 06154 VWS GROENLO Rit 24566 ',
-                               'Capcode': '0820154 MKA N-O Gelderland ( Ambulance 06-154 )\n',
-                               'Datum': '2022-01-24',
-                               'Prioriteit': 1,
-                               'Regio': 'Noord en Oost Gelderland',
-                               'Tijd': '12:45:09',
+    assert response.json() == {'ABP': 'AMBU',
+                               'Capcode': '1520999',
+                               'Datum': '2024-02-01',
+                               'Prioriteit': 2,
+                               'Regio': 'Haaglanden',
+                               'Tijd': '12:35:28',
                                'id': 12}
 
 def test_non_existing():
